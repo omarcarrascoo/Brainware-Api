3181188b52790f77c659d5602128730048472428
omarcarrascoo <omar@eraidigitalsolution.com>
2025-04-18 18:42:18 -0400

    adding the question for release

--- models/Questions.model.js
+++ models/Questions.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const questionSchema = new Schema({
+  ruleTitle: { type: String, required: true },
   ruleId: { type: String, required: true },
   q1: { type: String, required: true },
   q2: { type: String, required: true },
